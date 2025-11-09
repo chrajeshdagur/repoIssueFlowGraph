b7e8f9bec39edb041b35f335d5c3e45271b33470
Rajesh <chrajeshdagur@gmail.com>
2025-11-09 08:30:37 +0000

    bunch changes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Dashboard from './pages/Dashboard'
+import Maintenance from './pages/Maintenance'
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Navbar />
+        <main className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/maintenance" element={<Maintenance />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  )
+}
+
+export default App

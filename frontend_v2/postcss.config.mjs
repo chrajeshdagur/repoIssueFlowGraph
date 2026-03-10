db6edc388186e3381a78fca20c50c377acb09582
Rajesh <chrajeshdagur@gmail.com>
2026-03-10 12:33:57 +0000

    renamed folder to check git pages

--- frontend_v2/postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {}, // <--- Use this name as required by your new package versions
-    'autoprefixer': {},        // <-- Recommended to keep this for browser compatibility
-  },
-}
-
-export default config
